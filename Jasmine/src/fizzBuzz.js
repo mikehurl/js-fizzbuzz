8082bc0a928fcf1e6953c83e0a2d29b896bafd78
sara <sara@huhbub.com>
2016-12-07 15:59:31 +0000

    refactors isDivisibleBy method

--- Jasmine/src/fizzBuzz.js
+++ Jasmine/src/fizzBuzz.js
@@ -1,8 +1,8 @@
 function fizzBuzz() {
 
-  fizzBuzz.prototype.isDivisibleByThree = function(number) { return number % 3 === 0; }
-  fizzBuzz.prototype.isDivisiblebyFive = function(number) { return number % 5 === 0; }
-  fizzBuzz.prototype.isDivisibleByThreeAndFive = function(number) { return number % 15 === 0; }
+  fizzBuzz.prototype.isDivisibleByThree = function(number) { return this.isDivisibleBy(number,3); }
+  fizzBuzz.prototype.isDivisiblebyFive = function(number) { return this.isDivisibleBy(number,5);}
+  fizzBuzz.prototype.isDivisibleByThreeAndFive = function(number) { return this.isDivisibleBy(number,15); }
   fizzBuzz.prototype.isDivisibleBy = function(number, divisor) { return number % divisor === 0; }
 
 }
